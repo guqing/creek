63c479971359b4283cc71a0fff4a61d66182a407
guqing <1484563614@qq.com>
2019-12-21 16:41:01 +0800

    init commit

---
+++ src/main/java/xyz/guqing/springbootjpastarter/SpringbootJpaStarterApplication.java
@@ -0,0 +1,13 @@
+package xyz.guqing.springbootjpastarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/guqing/springbootjpastarter/SpringbootJpaStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.guqing.springbootjpastarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
