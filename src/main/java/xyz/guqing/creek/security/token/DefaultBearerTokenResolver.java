49886bd41094cbd8819b277f9971b22c016c9e54
guqing <1484563614@qq.com>
2022-01-19 17:49:10 +0800

    feat: 新增bearer token filter及相关类

---
+++ src/main/java/xyz/guqing/creek/security/token/DefaultBearerTokenResolver.java
@@ -0,0 +1,123 @@
+package xyz.guqing.creek.security.token;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
+import xyz.guqing.creek.security.BearerTokenError;
+import xyz.guqing.creek.security.BearerTokenErrors;
+import xyz.guqing.creek.security.BearerTokenResolver;
+
+/**
+ * The default {@link BearerTokenResolver} implementation based on RFC 6750.
+ *
+ * @author Vedran Pavic
+ * @since 5.1
+ * @see <a href="https://tools.ietf.org/html/rfc6750#section-2" target="_blank">RFC 6750
+ * Section 2: Authenticated Requests</a>
+ */
+public final class DefaultBearerTokenResolver implements BearerTokenResolver {
+
+    private static final Pattern authorizationPattern = Pattern.compile("^Bearer (?<token>[a-zA-Z0-9-._~+/]+=*)$",
+        Pattern.CASE_INSENSITIVE);
+
+    private boolean allowFormEncodedBodyParameter = false;
+
+    private boolean allowUriQueryParameter = false;
+
+    private String bearerTokenHeaderName = HttpHeaders.AUTHORIZATION;
+
+    @Override
+    public String resolve(final HttpServletRequest request) {
+        final String authorizationHeaderToken = resolveFromAuthorizationHeader(request);
+        final String parameterToken = isParameterTokenSupportedForRequest(request)
+            ? resolveFromRequestParameters(request) : null;
+        if (authorizationHeaderToken != null) {
+            if (parameterToken != null) {
+                final BearerTokenError error = BearerTokenErrors
+                    .invalidRequest("Found multiple bearer tokens in the request");
+                throw new OAuth2AuthenticationException(error);
+            }
+            return authorizationHeaderToken;
+        }
+        if (parameterToken != null && isParameterTokenEnabledForRequest(request)) {
+            return parameterToken;
+        }
+        return null;
+    }
+
+    /**
+     * Set if transport of access token using form-encoded body parameter is supported.
+     * Defaults to {@code false}.
+     * @param allowFormEncodedBodyParameter if the form-encoded body parameter is
+     * supported
+     */
+    public void setAllowFormEncodedBodyParameter(boolean allowFormEncodedBodyParameter) {
+        this.allowFormEncodedBodyParameter = allowFormEncodedBodyParameter;
+    }
+
+    /**
+     * Set if transport of access token using URI query parameter is supported. Defaults
+     * to {@code false}.
+     *
+     * The spec recommends against using this mechanism for sending bearer tokens, and
+     * even goes as far as stating that it was only included for completeness.
+     * @param allowUriQueryParameter if the URI query parameter is supported
+     */
+    public void setAllowUriQueryParameter(boolean allowUriQueryParameter) {
+        this.allowUriQueryParameter = allowUriQueryParameter;
+    }
+
+    /**
+     * Set this value to configure what header is checked when resolving a Bearer Token.
+     * This value is defaulted to {@link HttpHeaders#AUTHORIZATION}.
+     *
+     * This allows other headers to be used as the Bearer Token source such as
+     * {@link HttpHeaders#PROXY_AUTHORIZATION}
+     * @param bearerTokenHeaderName the header to check when retrieving the Bearer Token.
+     * @since 5.4
+     */
+    public void setBearerTokenHeaderName(String bearerTokenHeaderName) {
+        this.bearerTokenHeaderName = bearerTokenHeaderName;
+    }
+
+    private String resolveFromAuthorizationHeader(HttpServletRequest request) {
+        String authorization = request.getHeader(this.bearerTokenHeaderName);
+        if (!StringUtils.startsWithIgnoreCase(authorization, "bearer")) {
+            return null;
+        }
+        Matcher matcher = authorizationPattern.matcher(authorization);
+        if (!matcher.matches()) {
+            BearerTokenError error = BearerTokenErrors.invalidToken("Bearer token is malformed");
+            throw new OAuth2AuthenticationException(error);
+        }
+        return matcher.group("token");
+    }
+
+    private static String resolveFromRequestParameters(HttpServletRequest request) {
+        String[] values = request.getParameterValues("access_token");
+        if (values == null || values.length == 0) {
+            return null;
+        }
+        if (values.length == 1) {
+            return values[0];
+        }
+        BearerTokenError error = BearerTokenErrors.invalidRequest("Found multiple bearer tokens in the request");
+        throw new OAuth2AuthenticationException(error);
+    }
+
+    private boolean isParameterTokenSupportedForRequest(final HttpServletRequest request) {
+        return (("POST".equals(request.getMethod())
+            && MediaType.APPLICATION_FORM_URLENCODED_VALUE.equals(request.getContentType()))
+            || "GET".equals(request.getMethod()));
+    }
+
+    private boolean isParameterTokenEnabledForRequest(final HttpServletRequest request) {
+        return ((this.allowFormEncodedBodyParameter && "POST".equals(request.getMethod())
+            && MediaType.APPLICATION_FORM_URLENCODED_VALUE.equals(request.getContentType()))
+            || (this.allowUriQueryParameter && "GET".equals(request.getMethod())));
+    }
+
+}
